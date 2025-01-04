61878ca8e0f9e84569338d1bddcaa7a8cd19d5df
Kamil Rumanovsky <k.rumanovsky@gmail.com>
2025-01-04 20:17:29 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ HGLRC hRC = NULL; // main render device context
 #define TIMER_INDEX_SCENE 2		// next scene timer
 #define TIMER_TIME_FRAME 32		// 1000 / ms = fps
 #define TIMER_TIME_SCENE 300000 // next scene time (5 min)
-#define SCENE_COUNT 14			// number of scenes 0-13
+#define SCENE_COUNT 15			// number of scenes 0-14
 
 const wchar_t lpClassName[] = L"OpenGL_AL2D";
 const wchar_t lpWindowTitle[] = L"Artifical Life 2D";
@@ -365,6 +365,28 @@ static void glSceneUpdate()
 				c = o * e / 30 - t / 8;
 				px = q * .7 * sin(c) - 100;
 				py = y / 9 * cos(c * 4 - t / 2) - q / 2 * cos(c);
+				glVertex2d(px, py);
+			}
+			glEnd();
+		}
+		break;
+	case 14:
+		for (y = 0; y < 200; y++)
+		{
+			glBegin(GL_LINE_STRIP);
+			for (x = 0; x < 200; x++)
+			{
+				// color
+				alpha = 1 / (1 + abs(x - 100)) + 0.02f;
+				glColor4f(y / 200, 0.3f, 0.7f, alpha);
+				// draw
+				k = x / 8 - 12.5;
+				e = y / 8 - 12.5;
+				o = sqrt(pow(k, 2) + pow(e, 2)) / 12; // magnitude(k, e) / 12;
+				o *= cos(sin(k / 2) * cos(e / 2));
+				d = 5 * cos(o);
+				px = x + d * k * (sin(d * 2 + t) + sin(y * o * o) / 9) - 100;
+				py = y / 3 - d * 40 + 19 * cos(d + t) * 2 + 50;
 				glVertex2d(px, py);
 			}
 			glEnd();
